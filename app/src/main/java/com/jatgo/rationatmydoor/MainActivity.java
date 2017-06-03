51696db7a157baf8f096a2374a34561214da95ad
Jatin Goyal <goyal1205@gmail.com>
2017-06-03 19:01:15 +0530

    adding first screens

--- app/src/main/java/com/jatgo/rationatmydoor/MainActivity.java
+++ app/src/main/java/com/jatgo/rationatmydoor/MainActivity.java
@@ -1,7 +1,12 @@
 package com.jatgo.rationatmydoor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.jatgo.rationatmydoor.Home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button skipBtn = (Button)findViewById(R.id.button2);
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
